44ef398ee1efeedc44a10e469122ec0aff6eb20c
Son Jiui <77148574+J2020n21@users.noreply.github.com>
2023-04-23 19:29:45 +0900

    Initial commit
    Including the check whether I can get proper weather info through API(with https module).

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const https = require("https"); //http moduel to get GET request across the internet
+
+const app = express();
+
+app.get("/",function(req,res){
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Seoul&callback=test&appid=2959c9ba08becf152bbac4e5ae5820f0&units=metric";
+
+    https.get(url, function(response){
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            //console.log(data); //data printed in hexadecimal numbers
+            const weatherData = JSON.parse(data) //data(string) into actal js object.(json format)
+            //console.log(weatherData); //check parsed data
+            //make javascript into a string: JSON.stringify()
+
+           // const temp = weatherData.main.temp
+           // console.log(temp);
+
+        });
+    });
+
+    res.send("Running");
+});
+
+
+app.listen(3000,function(){
+    console.log("3000port:server running");
+});
